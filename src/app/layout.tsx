42ac904fe9a408d9d80ce73cd00a38bf395b8ae7
Mikeclassic <120394575+Mikeclassic@users.noreply.github.com>
2025-12-05 23:09:57 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
-import { ClerkProvider } from '@clerk/nextjs/server' // or @clerk/nextjs
-import './globals.css'  // <--- THIS LINE IS MISSING OR BROKEN!
+import { ClerkProvider } from '@clerk/nextjs'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
