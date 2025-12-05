56f2f286d1d2c019b6624bccf8ef3e002ca414b8
Mikeclassic <120394575+Mikeclassic@users.noreply.github.com>
2025-12-06 00:23:56 +0300

    Update daily-scrape.ts

--- scripts/daily-scrape.ts
+++ scripts/daily-scrape.ts
@@ -8,7 +8,7 @@ async function main() {
 
   // Launch the browser
   const browser = await puppeteer.launch({
-    headless: "new",
+    headless: "true",
     args: ['--no-sandbox', '--disable-setuid-sandbox'] // Required for GitHub Actions
   });
   
