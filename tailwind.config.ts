d4da7170e840b7257f049387d32912f3a7c68145
Mikeclassic <120394575+Mikeclassic@users.noreply.github.com>
2025-12-05 22:58:43 +0300

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: "hsl(var(--card))",
+        primary: "#8b5cf6",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
